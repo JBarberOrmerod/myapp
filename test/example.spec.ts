f8478fd78c5f694da7f628aa5199ba2a6cc419c4
John Barber-Ormerod <jack.barber.ormerod@gmail.com>
2022-11-13 22:36:29 -0500

    Unit testing with Vitest

---
+++ test/example.spec.ts
@@ -0,0 +1,22 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import HelloWorld from "../src/components/HelloWorld.vue"
+import Customers from "../src/views/CustomerView.vue"
+
+describe("HelloWorld.vue", () => {
+
+    it("renders props.msg when passed", () => {
+        const msg = "new message";
+        const wrapper = mount(HelloWorld, {
+          props: { msg },
+        });
+        expect(wrapper.text()).toMatch(msg);
+      });
+});
+
+describe("routing", () => {
+    it("Routes to a page", () => {
+      const wrapper = mount(Customers)
+      expect(wrapper.html()).toContain("Customer");
+    });
+  });
